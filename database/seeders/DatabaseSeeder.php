92b0a2521de994ab0aefbea7a175f5c7770373e4
nazrul <nazrul.dev@gmail.com>
2022-11-25 20:49:50 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Setting;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,7 +18,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        User::create([
+            'name' => 'Taufik',
+            'email' => 'taufik@admin.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role' => 'admin',
 
+        ]);
+        User::create([
+            'name' => 'master',
+            'email' => 'master@admin.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role' => 'admin',
+
+        ]);
+
+        User::create([
+            'name' => 'Yunus',
+            'email' => 'yunus@admin.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role' => 'admin',
+
+        ]);
         Setting::create([
             'key' => 'biaya_pendaftaran',
             'value' => '1500000',
